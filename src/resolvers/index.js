8935a0fa6655884b49bdf2e30ca9ebeb00bf8931
Marouane El Kachchani <marouane.elkachchani@gmail.com>
2021-03-09 12:22:53 +0100

    Add Subscriptions myAssignedRequestsAsCustomer, onHoldRequests

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,11 +1,13 @@
 import { extractFragmentReplacements } from 'prisma-binding'
 import Query from './Query'
 import Mutation from './Mutation'
+import Subscription from './Subscription'
 import User from './User'
 
 const resolvers = {
     Query,
     Mutation,
+    Subscription,
     User
 }
 
